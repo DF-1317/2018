c3ee9cc2c901b577cf1a824b057152a96310a13c
John Letourneau <jletourneau@cas.org>
2018-01-24 17:51:09 -0500

    Adding an example logging client

---
+++ programming/syslog_client/README.md
@@ -0,0 +1,16 @@
+# syslog_client
+
+This is used to demonstrate a tiny bit of the syslog4j functionality.
+To build this, use the command:
+```bash
+mvn package
+```
+
+## Running
+Since you will need the syslog4j package and your program's jar, the <b>run</b> script was included to help
+in getting the jar files and executing the test. Any argument you would give the Logger program you can pass in
+on the command line like so:
+I those cases do:
+```bash
+./run here is the message I want to send
+```
---
+++ programming/syslog_client/src/main/java/Logger.java
@@ -0,0 +1,78 @@
+import org.productivity.java.syslog4j.Syslog;
+import org.productivity.java.syslog4j.SyslogConstants;
+import org.productivity.java.syslog4j.SyslogIF;
+
+/*
+ * This code demonstrates using the syslog4j logging capability.
+ * Above are the imports needed to reference classes and handy constants.
+ * At the top of this class we have a couple handy constants of our own and a data member
+ * that will keep a reference to the Syslog instance doing all the work for us.
+ *
+ * This demo program needs Java 1.8 or above.
+ *
+ * What this logging framework allows for is the ability to have one to many systems and
+ * programs, all send their logging data to a single central server to be collected into a
+ * file. They will be formatted for us, have a timestamp, and an indicator as to what system
+ * they came from.
+ *
+ * For a robotics team, this can be helpful to get data off of the RoboRio [the robot control software]
+ * and also any other computers on the robot's network. For example if a Rasberry Pi were doing
+ * video processing of a camera input, that too could log to the same place the Rio was.
+ *
+ * You will need to adjust the ServerHost value below, depending on your environment. We do recommend
+ * using a static IP address on the robot network so that the servers are at known locations; DHCP has
+ * no guarantee of assigning the same IP to a system in successive runs.
+ *
+ * If your log server is a Linux box, like Ubuntu, you should configure the rsyslog facility to put
+ * local0 messages into their own file. Add the following to the /etc/rsyslog.d/50-default.conf
+ *  local0.*               /var/log/local0.log
+ *
+ * You will also want to relax the read permissions on files it creates and allow repeat messages.
+ * Check the following in /etc/rsyslog.conf
+ *  $FileCreateMode        0644
+ *  $DirCreateMode         0755
+ *  $RepeatedMsgReduction  off
+ */
+public class Logger {
+    static final String         ServerHost      = "10.40.3.212";                // address of the central log server
+    static final int            Facility        = SyslogIF.FACILITY_LOCAL0;     // what facility is labeled on the msg
+    SyslogIF                    sl;                                             // handle to the syslog framework
+
+    /*
+     * We get a handle to the syslog and declare that we are using UDP messaging; TCP is an option too,
+     * but would increase bandwidth use on the network. Given the topology of the robot's network, UDP
+     * is just fine.
+     */
+    public Logger() {
+        sl = Syslog.getInstance(SyslogConstants.UDP);
+        sl.getConfig().setFacility(Facility);
+        sl.getConfig().setHost(ServerHost);
+    }
+
+    /*
+     * Simply send the message and flush it out. The flush is nice to help keep the messages as real-time as
+     * possible; yeah, it is less efficient.
+     * We always log 'info' messages, there are other choices like: debug, error, warn, crit...
+     */
+    public void  log(String msg) {
+        System.out.println("Sending: " + msg);
+        sl.info(msg);
+        sl.flush();
+    }
+
+    /*
+     * Where execution starts. We tell the user we have started, then check to see if they gave
+     * us a message to send out.
+     * We create an instance of the logger class and log the message; they we are done.
+     */
+    public static void main(String[] args) {
+        System.out.println("Starting logger test...");
+
+        String msg = (0 < args.length) ? String.join(" ",args) : "Test msg";
+        Logger l = new Logger();
+        l.log(msg);
+
+        System.out.println("Logger done!");
+    }
+
+}
