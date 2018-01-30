534660de2ff0b52d521731f7ad0eaf834a3002a0
Local Laptop <local@laptop.com>
2018-01-30 18:33:53 -0500

    Finished Command to drive a certain number of inches.

--- RobotCommandBased2018/src/org/usfirst/frc/team1317/robot/commands/DriveInches.java
+++ RobotCommandBased2018/src/org/usfirst/frc/team1317/robot/commands/DriveInches.java
@@ -4,7 +4,6 @@ import org.usfirst.frc.team1317.robot.Robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -15,17 +14,18 @@ public class DriveInches extends Command {
 	final double WHEEL_CIRCUMFERENCE = 19;
 	final int ACCELERATION = 6000;
 	final int TICKS_PER_REVOLUTION = 4096;
+	final int ALLOWABLE_ERROR = 24; //allowable error in ticks
 	double rotations;
 	double targetPos;
-	int speed;
+	double speed;
 	WPI_TalonSRX FLMotor;
 	WPI_TalonSRX FRMotor;
 	
-    public DriveInches(double inches, int speed) {
+    public DriveInches(double inches, double speed) {
         requires(Robot.mecanumDriveTrain);
         rotations = inches / WHEEL_CIRCUMFERENCE;
         targetPos = TICKS_PER_REVOLUTION * rotations;
-        this.speed = speed; //speed is currently in sensor units/100 milliseconds
+        this.speed = speed/TICKS_PER_REVOLUTION * WHEEL_CIRCUMFERENCE * 1000; //speed is currently in sensor units/100 milliseconds
         //may possibly want to scale speed to other units
         FLMotor = Robot.mecanumDriveTrain.FLMotor;
         FRMotor = Robot.mecanumDriveTrain.FRMotor;
@@ -35,8 +35,9 @@ public class DriveInches extends Command {
     protected void initialize() {
     	Robot.mecanumDriveTrain.BRMotor.follow(FRMotor);
     	Robot.mecanumDriveTrain.BLMotor.follow(FLMotor);
-    	FLMotor.configMotionCruiseVelocity(speed,0);
-    	FRMotor.configMotionCruiseVelocity(speed,0);
+    	int intSpeed = (int) Math.round(speed);
+    	FLMotor.configMotionCruiseVelocity(intSpeed,0);
+    	FRMotor.configMotionCruiseVelocity(intSpeed,0);
     	FLMotor.configMotionAcceleration(ACCELERATION,0);
     	FRMotor.configMotionAcceleration(ACCELERATION,0);
     	FLMotor.setSelectedSensorPosition(0,0,0);
@@ -51,11 +52,20 @@ public class DriveInches extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
+    	if(Math.abs((FLMotor.getSensorCollection().getQuadraturePosition()-targetPos))<=ALLOWABLE_ERROR)
+    	{
+    		if(Math.abs((FRMotor.getSensorCollection().getQuadraturePosition()-targetPos))<=ALLOWABLE_ERROR)
+    			return true;
+    	}
         return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	FLMotor.set(ControlMode.PercentOutput, 0.0);
+    	FRMotor.set(ControlMode.PercentOutput, 0.0);
+    	Robot.mecanumDriveTrain.BRMotor.set(ControlMode.PercentOutput, 0.0);
+    	Robot.mecanumDriveTrain.BLMotor.set(ControlMode.PercentOutput, 0.0);
     }
 
     // Called when another command which requires one or more of the same
