2b8588843b02981e40e6511b03532d000f24ac0a
Local Laptop <local@laptop.com>
2018-01-31 20:49:47 -0500

    Minor adjustment to mecanum drive train

--- RobotCommandBased2018/src/org/usfirst/frc/team1317/robot/subsystems/MecanumDriveTrainCAN.java
+++ RobotCommandBased2018/src/org/usfirst/frc/team1317/robot/subsystems/MecanumDriveTrainCAN.java
@@ -102,6 +102,7 @@ public class MecanumDriveTrainCAN extends MecanumDriveTrain {
 	{
 		FLMotor.setSelectedSensorPosition(0,0,0);
     	FRMotor.setSelectedSensorPosition(0,0,0);
+	}
 
 	public void zeroGyro() {
 		navX.zeroYaw();
