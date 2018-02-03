67acb6dfc1de87944887860850fda527488c5f8e
legoh <legoh@DevanLaptop>
2018-02-03 11:04:55 -0500

    Set new ports for Robot

--- RobotCommandBased2018/src/org/usfirst/frc/team1317/robot/RobotMap.java
+++ RobotCommandBased2018/src/org/usfirst/frc/team1317/robot/RobotMap.java
@@ -17,10 +17,10 @@ public class RobotMap {
 	
 
 	//The ports for the drive train motors
-	public final static int FLMotorPort = 2;
-	public final static int FRMotorPort = 1;
-	public final static int BLMotorPort = 4;
-	public final static int BRMotorPort = 3;
+	public final static int FLMotorPort = 4;
+	public final static int FRMotorPort = 2;
+	public final static int BLMotorPort = 3;
+	public final static int BRMotorPort = 1;
 	
 	public final static int ElevatorPort = 5;
 	public final static int ArmMotor = 6;
