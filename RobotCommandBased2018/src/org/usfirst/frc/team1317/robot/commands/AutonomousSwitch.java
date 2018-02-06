ba547f5083e1cba311f048733931f9cea180f635
JonathanB05 <JonathanB.05@att.net>
2018-02-05 20:55:28 -0500

    Added autonomous code to place cube in switch from center position.

--- RobotCommandBased2018/src/org/usfirst/frc/team1317/robot/commands/AutonomousSwitch.java
+++ RobotCommandBased2018/src/org/usfirst/frc/team1317/robot/commands/AutonomousSwitch.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1317.robot.commands;
 
 import org.usfirst.frc.team1317.robot.Robot;
 
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -11,19 +12,40 @@ public class AutonomousSwitch extends CommandGroup {
 
     public AutonomousSwitch(String plateLocations, int startingPosition, boolean crossCourt, double delay) {
         
-    	Boolean SwitchLeft; 
+    	Command TurnLeft = new TurnDegrees(-90.0, 1.0);
+        Command TurnRight = new TurnDegrees(90.0, 1.0);
+    	Boolean SwitchLeft = null; 
     	if (plateLocations.charAt(0) == 'L')
     		SwitchLeft = true;
     	else if (plateLocations.charAt(0) == 'R')
     		SwitchLeft = false;
+    	addParallel( new PositionElevator(PositionElevator.SWITCH_POS));
     	addSequential( new Wait(delay) );
     	if(startingPosition == Robot.Center_Position)
     	{
-    		
+    		addSequential( new DriveInches(50.0, 70.0) );
+    		if(SwitchLeft)
+    			addSequential(TurnLeft);
+    		else
+    			addSequential(TurnRight);
+    		addSequential( new DriveInches(50.0, 70.0));
+    		if(SwitchLeft)
+    			addSequential(TurnRight);
+    		else
+    			addSequential(TurnLeft);
+    		addSequential( new DriveInches(50.0, 70.0) );
+    		addSequential( new PlaceCube());
     	}
     	else
     	{
-    		
+    		//if the switch and robot are on the same side
+    		if((startingPosition==Robot.Left_Position&&SwitchLeft)||(startingPosition==Robot.Right_Position&&!SwitchLeft)) {
+    			
+    		}
+    		//if the switch and robot are on opposite sides
+    		else {
+    			
+    		}
     	}
     }
 }
