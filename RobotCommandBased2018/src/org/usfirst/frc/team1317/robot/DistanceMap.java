75cf124476797cb814e4ea490cc0c3243d4e8f06
JonathanB05 <JonathanB.05@att.net>
2018-02-06 21:49:21 -0500

    Added Distances from Field Drawings

--- RobotCommandBased2018/src/org/usfirst/frc/team1317/robot/DistanceMap.java
+++ RobotCommandBased2018/src/org/usfirst/frc/team1317/robot/DistanceMap.java
@@ -1,8 +1,28 @@
 package org.usfirst.frc.team1317.robot;
 
 public class DistanceMap {
+	//all values are in inches.
 	
-	// These are constants for field distances to be used in autonomous
+	//These are constants that reflect actual field dimensions that can be used for calculating the paths if we want to be more flexible
+	//These are from https://firstfrc.blob.core.windows.net/frc2018/Drawings/LayoutandMarkingDiagram.pdf
+	public static final double WALL_TO_AUTO_LINE = 120.0;
+	public static final double WALL_TO_SWITCH = 144.0;
+	public static final double WALL_TO_FAR_SIDE_OF_SWITCH = 196.0;
+	public static final double WALL_TO_PLATFORM = 261.47;
+	public static final double WALL_TO_NULL_ZONE = 288.0;
+	public static final double NULL_ZONE_LENGTH = 72.0;
+	public static final double PLAYER_STATION_WIDTH = 264.0;
+	public static final double VAULT_WIDTH = 29.69; //the distance parallel to the player station wall from 
+	public static final double NULL_ZONE_WIDTH = 95.25;
+	public static final double SIDE_WALL_TO_SWITCH = 85.25;
+	
+	//these are robot constants. They will need to be changed
+	public static final double ROBOT_WIDTH = 32;
+	public static final double ROBOT_LENGTH = 32;
+	
+	
+	
+	// These are constants for field distances to be used in autonomous paths
 	public static final double MIDWAY_AUTO_LINE = 50.0;
 	public static final double AUTO_LINE = 100.0;
 	public static final double HORIZONTAL_PAST_SWITCH = 100.0;
