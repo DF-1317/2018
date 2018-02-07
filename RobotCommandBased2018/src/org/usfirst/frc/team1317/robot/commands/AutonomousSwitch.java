4b106b61eadcd6ec34c29c70c201d0dc9a8f489f
legoh <legoh@DevanLaptop>
2018-02-06 20:49:24 -0500

    Added constants to Autonomous Switch

--- RobotCommandBased2018/src/org/usfirst/frc/team1317/robot/commands/AutonomousSwitch.java
+++ RobotCommandBased2018/src/org/usfirst/frc/team1317/robot/commands/AutonomousSwitch.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team1317.robot.commands;
 
+import org.usfirst.frc.team1317.robot.DistanceMap;
 import org.usfirst.frc.team1317.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -23,45 +24,45 @@ public class AutonomousSwitch extends CommandGroup {
     	addSequential( new Wait(delay) );
     	if(startingPosition == Robot.Center_Position)
     	{
-    		addSequential( new DriveInches(50.0) );
+    		addSequential( new DriveInches(DistanceMap.MIDWAY_AUTO_LINE) );
     		addSequential( SwitchLeft ? TurnLeft : TurnRight);
-    		addSequential( new DriveInches(50.0));
+    		addSequential( new DriveInches(DistanceMap.HORIZONTAL_PAST_SWITCH));
     		if(SwitchLeft)
     			addSequential(TurnRight);
     		else
     			addSequential(TurnLeft);
-    		addSequential( new DriveInches(50.0) );
+    		addSequential( new DriveInches(DistanceMap.APPROACH_SWITCH_SIDE) );
     	}
     	else
     	{
     		//if the switch and robot are on the same side
     		if((startingPosition==Robot.Left_Position&&SwitchLeft)||(startingPosition==Robot.Right_Position&&!SwitchLeft)) {
-    			addSequential( new DriveInches(150.0) );
+    			addSequential( new DriveInches(DistanceMap.MIDWAY_AUTO_TO_SWITCH + DistanceMap.MIDWAY_AUTO_LINE) );
     			if(SwitchLeft) {
     				addSequential(TurnRight);
     			} else {
     				addSequential(TurnLeft);
     			}
-    			addSequential( new DriveInches(30.0) );
+    			addSequential( new DriveInches(DistanceMap.APPROACH_SWITCH) );
     		}
     		//if the switch and robot are on opposite sides
     		else {
     			if(crossFront) {
-	    			addSequential( new DriveInches(50.0) );
+	    			addSequential( new DriveInches(DistanceMap.MIDWAY_AUTO_LINE) );
 	    			addSequential( SwitchLeft ? TurnRight : TurnLeft );
-	    			addSequential( new DriveInches(300.0) );
+	    			addSequential( new DriveInches(DistanceMap.CROSS_COURT) );
 	    			addSequential( SwitchLeft ? TurnLeft : TurnRight );
-	    			addSequential( new DriveInches(100.0) );
+	    			addSequential( new DriveInches(DistanceMap.MIDWAY_AUTO_TO_SWITCH) );
 	    			addSequential( SwitchLeft ? TurnLeft : TurnRight );
     			} else {
-	    			addSequential( new DriveInches(250.0) );
+	    			addSequential( new DriveInches(DistanceMap.MIDWAY_AUTO_LINE + DistanceMap.MIDWAY_AUTO_TO_SWITCH + DistanceMap.SWITCH_TO_MIDWAY_SCALE) );
 	    			addSequential( SwitchLeft ? TurnRight : TurnLeft );
-	    			addSequential( new DriveInches(300.0) );
+	    			addSequential( new DriveInches(DistanceMap.CROSS_COURT) );
 	    			addSequential( SwitchLeft ? TurnRight : TurnLeft );
-	    			addSequential( new DriveInches(100.0) );
+	    			addSequential( new DriveInches(DistanceMap.SWITCH_TO_MIDWAY_SCALE) );
 	    			addSequential( SwitchLeft ? TurnRight : TurnLeft );
     			}
-    			addSequential( new DriveInches(30.0) );
+    			addSequential( new DriveInches(DistanceMap.APPROACH_SWITCH) );
     		}
     	}
     	// Always place the cube
