7ceedb5cd56a2e1a94de44594e7eb9340cd92470
Local Laptop <local@laptop.com>
2018-05-08 18:42:27 -0400

    Corrected encoder direction

--- RobotCommandBased2018/src/org/usfirst/frc/team1317/robot/RobotMap.java
+++ RobotCommandBased2018/src/org/usfirst/frc/team1317/robot/RobotMap.java
@@ -60,7 +60,7 @@ public class RobotMap {
 	public final static int AnalogGyroPort = 1;
 	
 	public static double RightEncoderDirection = 1.0;
-	public static double LeftEncoderDirection = -1.0;
+	public static double LeftEncoderDirection = 1.0;
 	
 	// If you are using multiple modules, make sure to define both the port
 	// number and the module. For example you with a rangefinder:
